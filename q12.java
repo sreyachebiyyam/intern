065ac1555fac994180d813db1008502d4887b15e
sreyachebiyyam <57995723+sreyachebiyyam@users.noreply.github.com>
2021-04-01 15:12:47 +0530

    Create q12.java

---
+++ q12.java
@@ -0,0 +1,14 @@
+class Grades
+{
+  public static void main(String args[])
+  {
+    Grades g=new Grades();
+    g.GradeAGirls();
+  }
+  void GradeAGirls()
+  {
+    int total=90,girls=45,boys=45,ga,ba=20;
+    ga=(total/2)-ba;
+    System.out.println("girls getting grade A" +ga);
+  }
+}
