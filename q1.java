6392db21b9bad06b3176b276be8aeee5e8a3a52c
sreyachebiyyam <57995723+sreyachebiyyam@users.noreply.github.com>
2021-04-01 14:04:24 +0530

    Create q1.java

---
+++ q1.java
@@ -0,0 +1,11 @@
+class Main
+{
+  public static void main(String args[])
+  {
+    int l=5,b=7,area,perimeter;
+    area=l*b;
+    perimeter=2*(l+b)
+    System.out.println("area of rectangle is",+area);
+    System.out.println("perimater of rectangle is",+perimeter);
+  }
+}
