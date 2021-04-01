323b35106aa2c47c72679c24f0273dc980184170
sreyachebiyyam <57995723+sreyachebiyyam@users.noreply.github.com>
2021-04-01 14:34:17 +0530

    Create q5.java

---
+++ q5.java
@@ -0,0 +1,16 @@
+import java.util.*;  
+public class Main
+{
+	public static void main(String[] args) 
+  {
+		int a=23,b=45;
+		if(a==b)
+		{
+		    System.out.println(a+" and "+b+" are equal.");
+		}
+		else
+		{
+		 System.out.println(a+" and "+b+" not are equal.");   
+		}
+	}
+}
