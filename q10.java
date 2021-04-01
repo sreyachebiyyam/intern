8bebe9c3702bd0bdce04447549aff26b4c944b5d
sreyachebiyyam <57995723+sreyachebiyyam@users.noreply.github.com>
2021-04-01 14:51:28 +0530

    Update q10.java

--- q10.java
+++ q10.java
@@ -1,14 +1,28 @@
 import java.lang.Math;
-public class Main
+public class Swap
 {
 	public static void main(String[] args) 
-  {
-		int a,b,t;
+  	{
+		int m,n;
+		Swap s=new Swap();
+		s.swapwithThirdvariable(m,n);
+		s.swapwithoutThirdVariable(m,n);
+	}
+	void swapwithThirdvariable(int a,int b)
+	{
+		int t;
 		System.out.println("Before Swapping a and b :"+a+" "+b);
 		t=a;
 		a=b;
 		b=t;
 		System.out.println("After Swapping a and b :"+a+" "+b);
 	}
-	
+	void swapwithoutThirdVariable()
+	{
+		System.out.println("Before Swapping a and b :"+a+" "+b);
+		a = a + b; 
+        	b = a - b;
+        	a = a - b;
+		System.out.println("After Swapping a and b :"+a+" "+b);
+	}
 }
