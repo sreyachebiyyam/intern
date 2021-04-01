6a2bb1f2323ea01908451705e67a0db904ac22e9
sreyachebiyyam <57995723+sreyachebiyyam@users.noreply.github.com>
2021-04-01 14:45:11 +0530

    Create q9.java

---
+++ q9.java
@@ -0,0 +1,13 @@
+import java.lang.Math;
+public class main
+{
+	public static void main(String[] args) 
+  {
+		int a,b,c,total;
+	    total=a+b+c;
+	    System.out.println("Total marks :"+total);
+	    double per=(total*100/300);
+	    System.out.println("percentage is :"+per);
+	}
+	
+}
