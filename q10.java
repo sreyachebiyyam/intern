012461cabf7a2a89c60b53c2892066e001f3f2b9
sreyachebiyyam <57995723+sreyachebiyyam@users.noreply.github.com>
2021-04-01 14:45:51 +0530

    Create q10.java

---
+++ q10.java
@@ -0,0 +1,14 @@
+import java.lang.Math;
+public class Main
+{
+	public static void main(String[] args) 
+  {
+		int a,b,t;
+		System.out.println("Before Swapping a and b :"+a+" "+b);
+		t=a;
+		a=b;
+		b=t;
+		System.out.println("After Swapping a and b :"+a+" "+b);
+	}
+	
+}
