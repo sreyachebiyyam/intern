7aa87d8956c3c557068c5e9ddf910cbba43e3367
sreyachebiyyam <57995723+sreyachebiyyam@users.noreply.github.com>
2021-04-01 14:38:22 +0530

    Create q8.java

---
+++ q8.java
@@ -0,0 +1,15 @@
+import java.lang.Math;
+public class main
+{
+	public static void main(String[] args) 
+	{
+		int a=55,b=70;
+	    if(a<50 || a<b)
+	    {
+	        System.out.println("True");
+	    }
+	    else
+	    {
+	       System.out.println("False");
+	    }
+	}
